85ecb441a873f02c6e69386089f7f26b53bf08e9
Utto <utto660@gmail.com>
2018-04-29 13:54:29 +0300

    fixing schema

--- graphql/executableSchema.js
+++ graphql/executableSchema.js
@@ -3,10 +3,11 @@ import { makeExecutableSchema } from 'graphql-tools';
 import schema from './schema.graphql';
 import createResolvers from './resolver';
 import Post from '../models/posts';
+import User from '../models/users';
 
 const executableSchema = makeExecutableSchema({
 	typeDefs: [schema],
-	resolvers: createResolvers({ Post }),
+	resolvers: createResolvers({ Post, User }),
 });
 
 export default executableSchema;
